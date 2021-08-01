5dd2415e3d55aa9a57507b0ba934ff959aeb5aed
Cookiehook <shaun.harrison@live.co.uk>
2021-08-01 02:07:21 +0100

    List Location working with Search and Pagination

--- src_rework/app/views/list.py
+++ src_rework/app/views/list.py
@@ -1,5 +1,9 @@
 from django.contrib.auth.decorators import login_required
+from django.db.models import Q
 from django.shortcuts import render
+
+from app.models.constants import Region
+from app.models.locations import Location
 
 
 @login_required()
@@ -26,5 +30,28 @@ def list_farms(request):
 @login_required()
 def list_locations(request, region):
     template_name = 'pages/list_locations.html'
-    context = {}
+    pagination = 25
+    page = int(request.GET.get("page", 0))
+    results = page * pagination
+
+    if 'search' not in request.GET or 'all' in request.GET:
+        locations = Location.objects.filter(region=Region.objects.get(name=region))[results:results + pagination]
+    else:
+        search_term = request.GET['search']
+        locations = Location.objects.filter(
+            Q(region=Region.objects.get(name=region)) & (Q(name__icontains=search_term) | Q(description__icontains=search_term)))
+
+    context = {
+        "search_placeholder": f"Search {region}...",
+        "locations": locations,
+        "search_suggestions": [l.name for l in locations]
+    }
+
+    if len(locations) == pagination:
+        context["next_page"] = page + 1
+    if page > 0:
+        context['previous_page'] = page - 1
+    if 'search' in request.GET and 'all' not in request.GET:
+        context['search_term'] = request.GET['search']
+
     return render(request, template_name, context)
